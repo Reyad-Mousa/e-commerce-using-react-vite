e09006c86d207212979da99793a0c03e474bf37d
Reyad-Mousa <99836425+Reyad-Mousa@users.noreply.github.com>
2024-03-31 08:56:47 +0200

    1-prepareProject , routing ,pages

---
+++ src/layout/MainLayout/MainLayout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import styles from "./styles.module.css";
+import Header from "@components/common/Header/Header";
+import Footer from "@components/common/Footer/Footer";
+const { container, wrapper } = styles;
+
+const MainLayout = () => {
+  return (
+    <Container className={container}>
+      <Header />
+      <div className={wrapper}>
+        <Outlet />
+      </div>
+      <Footer />
+    </Container>
+  );
+};
+
+export default MainLayout;
